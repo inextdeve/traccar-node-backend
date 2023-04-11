f03139115202baa00f4066628eeda640c4fe8113
Javascript <javascript@Javascripts-MacBook-Pro.local>
2023-04-11 23:06:00 +0000

    fix time query

--- src/api/controllers/statistics.js
+++ src/api/controllers/statistics.js
@@ -5,30 +5,22 @@ const kpi = async (req, res) => {
   const { query } = req;
   //Empted bins today
   const emptedBins = `SELECT COUNT(id) AS completed FROM tcn_poi_schedule
-                      WHERE serv_time BETWEEN "${query.from}" AND "${
-    query.from.split(" ")[0] + " 23:59"
-  }"`;
+                      WHERE serv_time BETWEEN "${query.from}" AND "${query.to}"`;
 
   //Washed bins today
   const washedBins = `SELECT COUNT(id) AS completed FROM tcn_posi_washing
-                      WHERE serv_time BETWEEN "${query.from}" AND "${
-    query.from.split(" ")[0] + " 23:59"
-  }"`;
+                      WHERE serv_time BETWEEN "${query.from}" AND "${query.to}"`;
 
   //Vehicle status
   const exitedVehicle = `SELECT count(DISTINCT tc_events.deviceid) AS completed from  tc_events
                           inner join tc_user_device on tc_events.deviceid = tc_user_device.deviceid
-                          where tc_events.eventtime BETWEEN "${
-                            query.from
-                          }" AND "${query.from.split(" ")[0] + " 23:59"}"
+                          where tc_events.eventtime BETWEEN "${query.from}" AND "${query.to}"
                           `;
 
   //Sweeper Status
   const exitedSweepers = `SELECT count(DISTINCT tc_events.deviceid) AS completed from  tc_events
                           inner join tc_devices on tc_events.deviceid = tc_devices.id
-                          WHERE tc_devices.groupid=5 AND tc_events.type = "geofenceExit" AND tc_events.eventtime BETWEEN "${
-                            query.from
-                          }" AND "${query.from.split(" ")[0] + " 23:59"}"
+                          WHERE tc_devices.groupid=5 AND tc_events.type = "geofenceExit" AND tc_events.eventtime BETWEEN "${query.from}" AND "${query.to}"
                           `;
 
   //Count All Bins
