f79ddbcc229428e9c1bc58af6a036993b159d413
Yassine Farroud <yassinefarroud@protonmail.com>
2023-05-08 21:56:01 +0300

    Fix timezone issues

--- src/api/routes/api.js
+++ src/api/routes/api.js
@@ -4,12 +4,13 @@ import washingRouter from "./washing.js";
 import devicesRouter from "./devices.js";
 import statisticsRouter from "./statistics.js";
 import auth from "../middlewares/auth.js";
+import moment from "moment";
 
 const router = express.Router();
 router.use(auth);
 router.use((req, _, next) => {
   if (!req.query.from)
-    req.query.from = new Date().toISOString().split("T")[0] + "T00:00";
+    req.query.from = moment().format().split("T")[0] + "T00:00";
   next();
 });
 
