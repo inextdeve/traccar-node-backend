979ed55ba6d0152c544c718901d78c9552d2e004
Yassine Farroud <yassinefarroud@protonmail.com>
2023-10-03 00:56:44 +0100

    index cert

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import cors from "cors";
 import sqlinjection from "sql-injection";
 import apiRouter from "./api/routes/api.js";
 import { db } from "./api/db/config/index.js";
+import https from "https";
+import fs from "fs";
+import path from "path";
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 dotenv.config();
 
@@ -19,6 +25,14 @@ app.use(sqlinjection); // add sql-injection middleware here
 
 //if db connection resolved listen to port 3003
 
-app.listen(3003, () => {
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app
+);
+
+sslServer.listen(3003, () => {
   console.log(`Server running on port ${3003}`);
 });
