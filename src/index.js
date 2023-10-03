119ff8c7eeb22b99dbbea68e5ad6c4363ab725eb
Yassine Farroud <yassinefarroud@protonmail.com>
2023-10-03 13:37:59 +0300

    remove tls

--- src/index.js
+++ src/index.js
@@ -3,36 +3,25 @@ import dotenv from "dotenv";
 import cors from "cors";
 import sqlinjection from "sql-injection";
 import apiRouter from "./api/routes/api.js";
-import { db } from "./api/db/config/index.js";
-import https from "https";
-import fs from "fs";
-import path from "path";
-import * as url from "url";
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "*", methods: "GET,PUT,POST,DELETE,PATCH" }));
+// Define your CORS options (customize to your needs)
+const corsOptions = {
+  origin: '*', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use("/api", apiRouter);
+app.use("/v2/api", apiRouter);
 
 app.use(sqlinjection); // add sql-injection middleware here
 
-//if db connection resolved listen to port 3003
-
-const sslServer = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
-  },
-  app
-);
-
-sslServer.listen(3003, () => {
+app.listen(3003, () => {
   console.log(`Server running on port ${3003}`);
 });
