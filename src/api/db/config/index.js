6fee9eb743d5113b578dbdda30c7c7a3832f66cf
Yassine Farroud <yassinefarroud@protonmail.com>
2025-01-19 23:22:31 +0100

    db name

--- src/api/db/config/index.js
+++ src/api/db/config/index.js
@@ -9,7 +9,7 @@ export default Object.freeze({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "rcj",
-    connectionLimit: 100
-  })
+    database: process.env.DB_NAME,
+    connectionLimit: 100,
+  }),
 });
